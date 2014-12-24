09fd12df06fded85144bdb4e910bcad26d49b982
ayushagarwal95 <agarwalayush161@gmail.com>
2014-12-24 13:40:53 +0530

    Success Stories Page Almost Done

--- js/navbar.js
+++ js/navbar.js
@@ -2,21 +2,12 @@
  * Created by Ayush Agarwal on 23/12/14.
  */
 $(document).ready(function () {
-    $('#display, #AboutUs, #AboutUsSmall').hover(
-        function () {
-            $(this).addClass('visible');
-            $('#AboutUs').show('slow');
-            $('#AboutUsSmall').show('slow');
-        },
-        function () {
-            $(this).removeClass('visible');
-            if ($('#display, #AboutUs, #AboutUsSmall').hasClass('visible')) {
-                return;
-            }
-            else {
-                $('#AboutUs').hide('slow');
-                $('#AboutUsSmall').hide('slow');
-            }
-        }
-    );
+    $(function () {
+        $.hovermenu({
+            selector: '#display, #display_small',
+            trigger: 'hover',
+            delay: 0,
+            autohide: true
+        });
+    });
 });
